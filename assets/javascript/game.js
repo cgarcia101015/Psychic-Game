aa475894314db553463f37c23da6a5c1e7b1a024
CarlosH <cgarcia101015@gmail.com>
2018-10-08 14:39:35 -0400

    random letter works

---
+++ assets/javascript/game.js
@@ -0,0 +1,8 @@
+var alphabet = "abcdefghijklmnopqrstuvwxyz";
+function getRandomLetter () {
+    return alphabet[Math.floor(Math.random() * alphabet.length)];
+}
+console.log(getRandomLetter());
+/*for (var i = 0; i < letters.length; i++) {
+    
+};*/
