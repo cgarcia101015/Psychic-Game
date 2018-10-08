60378c1512483cd65361a0deac8d30c340a659c3
CarlosH <cgarcia101015@gmail.com>
2018-10-08 15:52:35 -0400

    got keyevent to work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,21 @@
 var alphabet = "abcdefghijklmnopqrstuvwxyz";
+var randomLetter = getRandomLetter();
+
 function getRandomLetter () {
     return alphabet[Math.floor(Math.random() * alphabet.length)];
 }
-console.log(getRandomLetter());
+
+function keyFunction(event) {
+    var x = event;
+    return x;
+}
+console.log(randomLetter);
+
+window.addEventListener("keydown", event => {
+    if (event.key == randomLetter) {
+        document.body.style.background = "violet";
+    }
+});
 /*for (var i = 0; i < letters.length; i++) {
     
 };*/
